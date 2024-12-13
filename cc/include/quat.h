fa543548207cff1f165cc7b90667863235d4e6ec
Robert Schrom <rschrom.science@gmail.com>
2024-12-13 13:19:24 -0500

    restructured

---
+++ cc/include/quat.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+void multiply(std::vector<float> q1, std::vector<float> q2, std::vector<float> &q3);
+void conj(std::vector<float> q, std::vector<float> &cq);
---
+++ cc/src/quat.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <math.h>
+#include <vector>
+
+// mutliplication of quaternions
+void multiply(std::vector<float> q1, std::vector<float> q2, std::vector<float> &q3){
+  float r1 = q1[0], v1x = q1[1], v1y = q1[2], v1z = q1[3];
+  float r2 = q2[0], v2x = q2[1], v2y = q2[2], v2z = q2[3];
+  
+  q3[0] = r1*r2-v1x*v2x-v1y*v2y-v1z*v2z;
+  q3[1] = r1*v2x+r2*v1x+v1y*v2z-v1z*v2y;
+  q3[2] = r1*v2y+r2*v1y-v1x*v2z+v1z*v2x;
+  q3[3] = r1*v2z+r2*v1z+v1x*v2y-v1y*v2x;
+}
+
+// conjugate of quaternion
+void conj(std::vector<float> q, std::vector<float> &cq){
+  cq[0] = q[0];
+  cq[1] = -q[1];
+  cq[2] = -q[2];
+  cq[3] = -q[3];
+}
